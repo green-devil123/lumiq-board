8c7e2b007578bb6e28e5d26ca7da9abbc2863b21
Tarun Agarwal <agarwal.tarun90@gmail.com>
2021-03-14 20:22:04 +0530

    drag&drop fixed in horizontal and vertical both

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {CdkDragDrop} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  
+export class AppComponent implements OnInit {
+  colors = [];
+  boxColor=['violet','indigo','blue','green','yellow','orange','red','pink'];
+
+  drop(event: CdkDragDrop<any>) {
+    this.colors[event.previousContainer.data.index]=event.container.data.item;
+    this.colors[event.container.data.index]=event.previousContainer.data.item;
+  }
+
+  ngOnInit(){
+    this.colors = [...this.boxColor,...this.boxColor,...this.boxColor,...this.boxColor,...this.boxColor,...this.boxColor,...this.boxColor,...this.boxColor];
+  }
 }
+ 
